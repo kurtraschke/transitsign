c4f20afd732b5512eed382a08c9c42903030ece5
Kurt Raschke <kurt@kurtraschke.com>
2011-08-13 20:05:47 -0400

    Automatically size list of buses based on window size.

--- static/modules/bus.js
+++ static/modules/bus.js
@@ -6,13 +6,22 @@ var bus = {
   doInit: function(div, socket) {
     div = div[0];
 
+    //render dummy element for sizing
+    soy.renderElement(div, busTemplate.main,
+        {'buses': [{'Minutes': 10, 'Agency': 'Test Agency',
+          'RouteID': 'Route', 'StopName': 'Stop Name',
+          'DirectionText': 'Direction Text'}]});
+
+    numBuses = Math.floor(($(window).height() - $('#header').outerHeight()) /
+                          $(div).children(':first').outerHeight()) * 2;
+
     socket.emit('set buses', config.stops);
 
     function updateBuses() {
       socket.emit('get buses', function(response) {
         response = response.buses;
         soy.renderElement(div, busTemplate.main,
-                          {'buses': response.slice(0, 8)});
+                          {'buses': response.slice(0, numBuses)});
       });
     }
 
@@ -23,8 +32,6 @@ var bus = {
   onShow: function() {},
   onHide: function() {},
   title: function() {return 'Buses near here';}
-
-
-
-
 };
+
+var numBuses = 0;
