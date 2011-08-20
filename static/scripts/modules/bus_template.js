fb9aed3064ce9d95433897be91d305fa264414f7
Kurt Raschke <kurt@kurtraschke.com>
2011-08-20 14:49:39 -0400

    Create distinction between bus directions and destinations.
    Some APIs report both a direction ('Westbound') and a destination ('Airport'), while others report only one or the other.  We now support all three possible cases.

--- static/scripts/modules/bus_template.js
+++ static/scripts/modules/bus_template.js
@@ -13,11 +13,11 @@ busTemplate.main = function(opt_data, opt_sb) {
 
 busTemplate.predictions = function(opt_data, opt_sb) {
   var output = opt_sb || new soy.StringBuilder();
-  var busList49 = opt_data.buses;
-  var busListLen49 = busList49.length;
-  for (var busIndex49 = 0; busIndex49 < busListLen49; busIndex49++) {
-    var busData49 = busList49[busIndex49];
-    output.append('<div class="busprediction"><div class="minutes">', soy.$$escapeHtml(busData49.Minutes), '</div><div><span class="route">', soy.$$escapeHtml(busData49.Agency), ' ', soy.$$escapeHtml(busData49.RouteID), '</span><br>', soy.$$escapeHtml(busData49.StopName), '<br>', soy.$$escapeHtml(busData49.DirectionText), '</div></div>');
+  var busList7 = opt_data.buses;
+  var busListLen7 = busList7.length;
+  for (var busIndex7 = 0; busIndex7 < busListLen7; busIndex7++) {
+    var busData7 = busList7[busIndex7];
+    output.append('<div class="busprediction"><div class="minutes">', soy.$$escapeHtml(busData7.Minutes), '</div><div><span class="route">', soy.$$escapeHtml(busData7.Agency), ' ', soy.$$escapeHtml(busData7.RouteID), '</span><br>', soy.$$escapeHtml(busData7.StopName), '<br>', (busData7.DirectionText && busData7.DestinationName) ? '<span class="direction">' + soy.$$escapeHtml(busData7.DirectionText) + '</span> to <span class="destination">' + soy.$$escapeHtml(busData7.DestinationName) + '</span>' : (busData7.DirectionText) ? '<span class="direction">' + soy.$$escapeHtml(busData7.DirectionText) + '</span>' : (busData7.DestinationName) ? '<span class="destination">' + soy.$$escapeHtml(busData7.DestinationName) + '</span>' : '', '</div></div>');
   }
   if (!opt_sb) return output.toString();
 };
