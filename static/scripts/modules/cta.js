23466b83abded39217d3d9e2ea7a5660c28777b4
Kurt Raschke <kurt@kurtraschke.com>
2011-08-30 21:32:45 -0400

    Add marquee for CTA rail incidents.

--- static/scripts/modules/cta.js
+++ static/scripts/modules/cta.js
@@ -1,5 +1,5 @@
-define(['jquery', 'soy', '../tools', './cta_template'],
-       function(_jquery, _soy, tools, _template) {
+define(['jquery', 'marquee', 'soy', '../tools', './cta_template'],
+       function(_jquery, _marquee, _soy, tools, _template) {
 
       CTASlide.instanceCount = 1;
 
@@ -21,12 +21,30 @@ define(['jquery', 'soy', '../tools', './cta_template'],
            $('.railpredictions tbody', self.div)[0],
            ctaTemplate.predictions,
            {predictions: [{rt: 'Blue', destNm: 'O\'Hare', min: 3}]},
-           0);
+           6.5 * tools.emSize($('body')));
+
+        this.marquee = $('.incidents', this.div);
+
+        this.marquee.marquee({
+          yScroll: 'bottom', pauseSpeed: 1500,
+          scrollSpeed: 8, pauseOnHover: false,
+          beforeshow: function($marquee, $li) {
+            var lines = $li.find('.lines');
+            $('.lnbox', self.div).html(lines.html()).fadeIn(1000);
+          },
+          aftershow: function($marquee, $li) {
+            //$('.lnbox', self.div).hide();
+          }
+        });
+
+        this.marquee.data('state', 'running');
 
         socket.emit('subscribe cta stop', this.parameters.mapid);
 
         self.updateTrains();
+        self.updateIncidents();
         setInterval(function() {self.updateTrains();}, 60000);
+        setInterval(function() {self.updateIncidents();}, 120000);
       }
 
 
@@ -40,6 +58,44 @@ define(['jquery', 'soy', '../tools', './cta_template'],
              ctaTemplate.predictions,
              {'predictions': response.trains.slice(0, self.numTrains)});
            });
+      };
+
+
+      CTASlide.prototype.onHide = function() {
+        //stop marquee
+        this.marquee.marquee('pause');
+        this.marquee.data('state', 'paused');
+      };
+
+      CTASlide.prototype.onShow = function() {
+        //start marquee
+        this.marquee.marquee('resume');
+        this.marquee.data('state', 'running');
+      };
+
+      CTASlide.prototype.updateIncidents = function() {
+        var self = this;
+        this.socket.emit('get cta incidents',
+           function(response) {self.setIncidents(response);});
+      };
+
+      CTASlide.prototype.setIncidents = function(response) {
+        if (this.marquee.data('state') === 'running') {
+          this.marquee.marquee('pause');
+        }
+        $('.lnbox', this.div).html('');
+        if (response.incidents.length === 0) {
+          this.marquee.html('<li></li>');
+        } else {
+          this.marquee.html(
+             ctaTemplate.incidents({'incidents': response.incidents})
+          );
+        }
+        this.marquee.marquee('update');
+        this.marquee.marquee('resume');
+        if (this.marquee.data('state') === 'paused') {
+          this.marquee.marquee('pause');
+        }
       };
 
       return CTASlide;
--- static/scripts/modules/cta_template.js
+++ static/scripts/modules/cta_template.js
@@ -6,21 +6,21 @@ if (typeof ctaTemplate == 'undefined') { var ctaTemplate = {}; }
 
 ctaTemplate.main = function(opt_data, opt_sb) {
   var output = opt_sb || new soy.StringBuilder();
-  output.append('<table class="railpredictions"><thead><tr><td>Line</td><td>Destination</td><td>Minutes</td></tr></thead><tbody></tbody></table></div>');
+  output.append('<table class="railpredictions"><thead><tr><td>Line</td><td>Destination</td><td>Minutes</td></tr></thead><tbody></tbody></table><div class="ic"><div class="lnbox"></div><ul class="incidents"></ul></div>');
   if (!opt_sb) return output.toString();
 };
 
 
 ctaTemplate.predictions = function(opt_data, opt_sb) {
   var output = opt_sb || new soy.StringBuilder();
-  var predictionList66 = opt_data.predictions;
-  var predictionListLen66 = predictionList66.length;
-  for (var predictionIndex66 = 0; predictionIndex66 < predictionListLen66; predictionIndex66++) {
-    var predictionData66 = predictionList66[predictionIndex66];
+  var predictionList5 = opt_data.predictions;
+  var predictionListLen5 = predictionList5.length;
+  for (var predictionIndex5 = 0; predictionIndex5 < predictionListLen5; predictionIndex5++) {
+    var predictionData5 = predictionList5[predictionIndex5];
     output.append('<tr><td>');
-    ctaTemplate.line({line: predictionData66.rt}, output);
-    output.append('</td><td>', soy.$$escapeHtml(predictionData66.destNm), '</td><td>');
-    ctaTemplate.time({prediction: predictionData66}, output);
+    ctaTemplate.line({line: predictionData5.rt}, output);
+    output.append('</td><td>', soy.$$escapeHtml(predictionData5.destNm), '</td><td>');
+    ctaTemplate.time({prediction: predictionData5}, output);
     output.append('</td></tr>');
   }
   if (!opt_sb) return output.toString();
@@ -64,5 +64,31 @@ ctaTemplate.line = function(opt_data, opt_sb) {
       break;
   }
   output.append('</span>');
+  if (!opt_sb) return output.toString();
+};
+
+
+ctaTemplate.incidents = function(opt_data, opt_sb) {
+  var output = opt_sb || new soy.StringBuilder();
+  var incidentList48 = opt_data.incidents;
+  var incidentListLen48 = incidentList48.length;
+  for (var incidentIndex48 = 0; incidentIndex48 < incidentListLen48; incidentIndex48++) {
+    var incidentData48 = incidentList48[incidentIndex48];
+    output.append('<li><span class="lines">');
+    ctaTemplate.services({services: incidentData48.ImpactedService.Service}, output);
+    output.append(' Alert: </span>', soy.$$escapeHtml(incidentData48.ShortDescription), '</li>');
+  }
+  if (!opt_sb) return output.toString();
+};
+
+
+ctaTemplate.services = function(opt_data, opt_sb) {
+  var output = opt_sb || new soy.StringBuilder();
+  var serviceList57 = opt_data.services;
+  var serviceListLen57 = serviceList57.length;
+  for (var serviceIndex57 = 0; serviceIndex57 < serviceListLen57; serviceIndex57++) {
+    var serviceData57 = serviceList57[serviceIndex57];
+    output.append((serviceData57.ServiceType == 'R') ? '<span class="colorblock ' + serviceData57.ServiceId + '">' + ((serviceData57.ServiceId == 'Pexp') ? '<span class="exp">EXP</span>' : '') + '</span>' : '');
+  }
   if (!opt_sb) return output.toString();
 };
