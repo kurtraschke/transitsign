kurtraschke/transitsign

commit f6b9f87b393bb69a800df211a7dd008a760d561c

Refactor fetching to eliminate duplicate code.

lib/plugins/sources/wmata/incidents.js
@@ -1,28 +1,17 @@
 var async = require('async');
 
-var rlrequest = require('../rlrequest');
+var fetch = require('../fetch');
 var keymaster = require('../../../keymaster');
 
 module.exports = updateIncidents;
 
 function updateIncidents(db, callback) {
+  var url = 'http://api.wmata.com/StationPrediction.svc/json/' +
+      'GetPrediction/all?api_key=' + keymaster.keyForAgency('WMATA');
   async.waterfall([
-    function(callback) {
-      var url = 'http://api.wmata.com/Incidents.svc/json/Incidents?api_key=' +
-          keymaster.keyForAgency('WMATA');
-      rlrequest.request_limited({uri: url},
-          function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-              var parsed = JSON.parse(body);
-              callback(null, parsed);
-            } else {
-              callback({'error': error, 'response': response});
-            }
-          }
-      );
-    },
+    fetch.fetch(url, 'json'),
     function(parsed_data, callback) {
-      var incidents = parsed_data.Incidents;
+      var incidents = parsed_data.Incidents || [];
       var incidentsOut = Array();
 
       for (var i = 0, l = incidents.length; i < l; i++) {

lib/plugins/sources/bart/incidents.js
@@ -1,36 +1,15 @@
-var xml2js = require('xml2js');
 var async = require('async');
 
-var rlrequest = require('../rlrequest');
+var fetch = require('../fetch');
 var keymaster = require('../../../keymaster');
 
 module.exports = updateIncidents;
 
 function updateIncidents(db, callback) {
+  var url = 'http://api.bart.gov/api/bsa.aspx?cmd=bsa&key=' +
+      keymaster.keyForAgency('BART');
   async.waterfall([
-    function(callback) {
-      var url = 'http://api.bart.gov/api/bsa.aspx?cmd=bsa&key=' +
-                    keymaster.keyForAgency('BART');
-      rlrequest.request_limited({uri: url},
-          function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-              callback(null, body);
-            } else {
-              callback({'error': error, 'response': response});
-            }
-          }
-      );
-    },
-    function(body, callback) {
-      var parser = new xml2js.Parser();
-      parser.on('end', function(result) {
-        callback(null, result);
-      });
-      parser.on('error', function(err) {
-        callback(err);
-      });
-      parser.parseString(body);
-    },
+    fetch.fetch(url, 'xml'),
     function(parsed_data, callback) {
       var incidents = parsed_data.bsa;
 

lib/plugins/sources/bart/trains.js
@@ -1,37 +1,16 @@
-var xml2js = require('xml2js');
 var async = require('async');
 
-var rlrequest = require('../rlrequest');
+var fetch = require('../fetch');
 var keymaster = require('../../../keymaster');
 
 
 module.exports = updateTrains;
 
 function updateTrains(db, callback) {
+  var url = 'http://api.bart.gov/api/etd.aspx?cmd=etd&orig=ALL&key=' +
+      keymaster.keyForAgency('BART');
   async.waterfall([
-    function(callback) {
-      var url = 'http://api.bart.gov/api/etd.aspx?cmd=etd&orig=ALL&key=' +
-                    keymaster.keyForAgency('BART');
-      rlrequest.request_limited({uri: url},
-          function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-              callback(null, body);
-            } else {
-              callback({'error': error, 'response': response});
-            }
-          }
-      );
-    },
-    function(body, callback) {
-      var parser = new xml2js.Parser();
-      parser.on('end', function(result) {
-        callback(null, result);
-      });
-      parser.on('error', function(err) {
-        callback(err);
-      });
-      parser.parseString(body);
-    },
+    fetch.fetch(url, 'xml'),
     function(parsed_data, callback) {
       var stations = parsed_data.station;
       var trainsOut = Array();

lib/plugins/sources/cta/alerts.js
@@ -1,36 +1,15 @@
 var async = require('async');
 var winston = require('winston');
-var xml2js = require('xml2js');
 
-var rlrequest = require('../rlrequest');
+var fetch = require('../fetch');
 
 module.exports = updateIncidents;
 
 function updateIncidents(db) {
-  async.waterfall([
-    function(callback) {
-      var url = 'http://www.transitchicago.com/api/1.0/alerts.aspx' +
+  var url = 'http://www.transitchicago.com/api/1.0/alerts.aspx' +
                 '?activeonly=true';
-      rlrequest.request_limited({uri: url},
-          function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-              callback(null, body);
-            } else {
-              callback({'error': error, 'response': response});
-            }
-          }
-      );
-    },
-    function(body, callback) {
-      var parser = new xml2js.Parser();
-      parser.on('end', function(result) {
-        callback(null, result);
-      });
-      parser.on('error', function(err) {
-        callback(err);
-      });
-      parser.parseString(body);
-    },
+  async.waterfall([
+    fetch.fetch(url, 'xml'),
     function(parsed_data, callback) {
       if (parsed_data.ErrorCode !== '0') {
         if (parsed_data.ErrorCode == '25' || parsed_data.ErrorCode == '50') {

lib/plugins/sources/cta/rail.js
@@ -1,9 +1,8 @@
 var async = require('async');
 var winston = require('winston');
-var xml2js = require('xml2js');
 
 
-var rlrequest = require('../rlrequest');
+var fetch = require('../fetch');
 var keymaster = require('../../../keymaster');
 var subscriptions = require('../../../subscriptions');
 
@@ -27,29 +26,10 @@ function updateTrains(db) {
 
 function fetchTrains(mapid, db, callback) {
   var key = keymaster.keyForAgency('CTARail');
+  var url = URLForStop(key, mapid);
+
   async.waterfall([
-    function(callback) {
-      var url = URLForStop(key, mapid);
-      rlrequest.request_limited({uri: url},
-          function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-              callback(null, body);
-            } else {
-              callback({'error': error, 'response': response});
-            }
-          }
-      );
-    },
-    function(body, callback) {
-      var parser = new xml2js.Parser();
-      parser.on('end', function(result) {
-        callback(null, result);
-      });
-      parser.on('error', function(err) {
-        callback(err);
-      });
-      parser.parseString(body);
-    },
+    fetch.fetch(url, 'xml'),
     function(parsed_data, callback) {
       if (parsed_data.errCd !== '0') {
         callback({'error': parsed_data.errNm});

lib/plugins/sources/cumtd/cumtd.js
@@ -1,26 +1,15 @@
 var async = require('async');
 
-var rlrequest = require('../rlrequest');
+var fetch = require('../fetch');
 var keymaster = require('../../../keymaster');
 var timezone = require('../../../ext/timezone');
 
 module.exports = updateCUMTDPredictions;
 
 function updateCUMTDPredictions(agency, stopID, db, callback) {
+  var url = URLForStop(keymaster.keyForAgency('CUMTD'), stopID);
   async.waterfall([
-    function(callback) {
-      rlrequest.request_limited({uri: URLForStop(
-          keymaster.keyForAgency('CUMTD'), stopID)
-      },
-      function(error, response, body) {
-        if (!error && response.statusCode == 200) {
-          var parsed = JSON.parse(body);
-          callback(null, parsed);
-        } else {
-          callback({'error': error, 'response': response});
-        }
-      });
-    },
+    fetch.fetch(url, 'json'),
     function(parsed, callback) {
       if (parsed.stat != 'ok') {
         callback({'error': parsed.err});

lib/plugins/sources/cumtd/stops.js
@@ -1,6 +1,6 @@
 var async = require('async');
 
-var rlrequest = require('../rlrequest');
+var fetch = require('../fetch');
 var keymaster = require('../../../keymaster');
 
 module.exports = loadCUMTDStops;
@@ -13,17 +13,7 @@ function loadCUMTDStops(db, callback) {
                 '/stops.getList?key=' + key;
 
   async.waterfall([
-    function(callback) {
-      rlrequest.request_limited({uri: url},
-          function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-              var parsed = JSON.parse(body);
-              callback(null, parsed);
-            } else {
-              callback({'error': error, 'response': response});
-            }
-          });
-    },
+    fetch.fetch(url, 'json'),
     function(parsed, callback) {
       if (parsed.stat != 'ok') {
         callback({'error': parsed.err});

lib/plugins/sources/fetch.js
@@ -1,0 +1,45 @@
+var xml2js = require('xml2js');
+
+var rlrequest = require('./rlrequest');
+
+exports.fetch = fetch;
+
+function fetch(url, format) {
+  return function(callback) {
+    rlrequest.request_limited({uri: url},
+        function(error, response, body) {
+          if (!error && response.statusCode == 200) {
+            switch (format) {
+              case 'json':
+                var parsed = JSON.parse(body);
+                callback(null, parsed);
+                break;
+              case 'xml':
+                var parser = new xml2js.Parser();
+                parser.on('end', function(result) {
+                  callback(null, result);
+                });
+                parser.on('error', function(error) {
+                  callback(error);
+                });
+                parser.parseString(body);
+                break;
+              default:
+                callback(null, body);
+            }
+          } else {
+            var meta = {};
+            if (error) {
+              meta['error'] = error;
+            }
+            if (response) {
+              meta['statusCode'] = response.statusCode;
+              meta['headers'] = response.headers;
+              meta['body'] = response.body;
+            }
+            callback(meta);
+          }
+        }
+    );
+  };
+}

commit edc193c86cc30aff1ab980f1a74f229a2bdcf461

Correct bug introduced in f6b9f87b which caused WMATA incidents not to be fetched.

lib/plugins/sources/wmata/incidents.js
@@ -6,8 +6,8 @@ var keymaster = require('../../../keymaster');
 module.exports = updateIncidents;
 
 function updateIncidents(db, callback) {
-  var url = 'http://api.wmata.com/StationPrediction.svc/json/' +
-      'GetPrediction/all?api_key=' + keymaster.keyForAgency('WMATA');
+  var url = 'http://api.wmata.com/Incidents.svc/json/Incidents?api_key=' +
+    keymaster.keyForAgency('WMATA');
   async.waterfall([
     fetch.fetch(url, 'json'),
     function(parsed_data, callback) {